09fce631565ca926741baaeb0a0e009f541322f8
aliifam <aliif.arief.m@gmail.com>
2023-06-14 22:08:46 +0700

    init project

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Project 13 - User Frontend
+
+using react native for displaying list of rooms and chair for booking a session
+
+- React Native
+- Firebase
+- Axios
+- React Navigation
---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen woi agha</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View>
+      <Text>Details Screen anjaygak</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
